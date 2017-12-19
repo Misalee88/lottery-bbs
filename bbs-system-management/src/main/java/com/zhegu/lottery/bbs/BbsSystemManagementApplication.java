0bfa0f1002586044089ff405e7dc11e332b8fa32
Misalee88 <309667042@qq.com>
2017-12-19 13:47:53 +0800

    框架设计

--- bbs-system-management/src/main/java/com/zhegu/lottery/bbs/BbsSystemManagementApplication.java
+++ bbs-system-management/src/main/java/com/zhegu/lottery/bbs/BbsSystemManagementApplication.java
@@ -1,11 +1,9 @@
 package com.zhegu.lottery.bbs;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.zhegu.lottery.bbs.dao")
 public class BbsSystemManagementApplication{
 
 	public static void main(String[] args) {
